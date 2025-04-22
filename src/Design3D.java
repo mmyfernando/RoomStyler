d9c678ef0a47ae705a2a9d41e4f07961e5457791
Hashani2002 <148967088+Hashani2002@users.noreply.github.com>
2025-04-22 21:22:45 +0530

    3D Design added

---
+++ src/Design3D.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Design3D extends JFrame {
+    JPanel mainPanel;
+    private JButton titleBtn;
+    private JButton a3DDesignButton;
+
+    public Design3D() {
+        titleBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        a3DDesignButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
